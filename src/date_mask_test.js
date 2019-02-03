f79d6fd25085af42d2e58639e1c81027512416df
anotherDr <zerkaloreki@gmail.com>
2019-02-03 19:44:44 +0300

    v. 0.5.2 move test into outer file

---
+++ src/date_mask_test.js
@@ -0,0 +1,27 @@
+
+/* TEST -------------------------------------- */
+import AMask from "./amask";
+
+const testVal = [
+	{val: '0', res: '0_.__.____'},
+	{val: '00', res: '00.__.____'},
+	{val: '1.02.', res: '10.2_.____'},
+	{val: '1.02.1965', res: '10.21.965_'},
+	{val: '01.02.196589', res: '01.02.1965'},
+	{val: 'a', res: '__.__.____'},
+	{val: 'asdasdasd', res: '__.__.____'},
+	{val: '', res: '__.__.____'},
+];
+
+function test(){
+	let testMasc = new AMask();
+	
+	testVal.forEach((it, idx)=>{
+		let res = testMasc.aMaskCore(it.val);
+		console.log( idx, it.res === res, it.val , it.res, res );
+	});
+}
+
+if (window.location.hash === '#test') {
+	test();
+}
